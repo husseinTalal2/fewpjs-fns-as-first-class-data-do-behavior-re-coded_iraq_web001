2e829e49edf036b87f4e2e3c663ed58c116fd2e9
Hussein Talal <hossientalal2@gmail.com>
2020-05-14 19:50:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time){
+  timeDivide = time.splite(":");
+  if (timeDivide[0].parseInt()<12) {
+    return "Good Morning";
+  } else if(timeDivide[0].parseInt()<17 && timeDivide[0]>=12) {
+    return "Good Afternoon";
+  }
+  else if (timeDivide[0].parseInt()>=17) {
+    return "Good Evening";
+  } 
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(greetMsg){
+  greetMsg = greet()
+  let greeting = document.getElementById('greeting');
+  greeting.textContent = greetMsg;
+}
